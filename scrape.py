0630a48d908e48bf4d4e2fa3ddf2ce421b6bfd8b
Justin <7772287+faalqt@users.noreply.github.com>
2024-09-15 15:56:00 -0400

    Update scrape.py
    refactored and removed some stuff

--- scrape.py
+++ scrape.py
@@ -7,18 +7,6 @@ def fetch_soup(url):
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
-#i think this will be useless as I plan to re-create this in the SQL db
-def fetch_driver_standings(year):
-    soup = fetch_soup(f'https://www.formula1.com/en/results/{year}/drivers')
-    driver_standings = soup.find_all('tr', ['bg-brand-white', 'bg-grey-10'])
-    driver_standings.pop(0)
-
-    
-    for element in driver_standings:
-        pos = element.find('p').text
-        driver = element.find_all('span', ['max-desktop:hidden', 'max-tablet:hidden'])
-        print(pos, driver)
-
 #returns the races for the year
 def fetch_races(year):
     race_list_url = f'https://www.formula1.com/en/results/{year}/races'
@@ -28,25 +16,17 @@ def fetch_races(year):
     races = []
     for race in year_race_list:
         row = race.find_all('p', ['f1-text font-titillium tracking-normal font-normal non-italic normal-case leading-none f1-text__micro text-fs-15px'])
-        race_name = row[0].text
         race_link = row[0].find('a')['href']
-        race_id = race_link[6:race_link.find('/', 6, len(race_link)-1)]
-        race_date = row[1].text
-        race_winner = row[2].text[0:len(row[2].text)-3].replace(u'\xa0', ' ')
-        race_winner_tag = row[2].text[len(row[2].text)-3::]
-        constructor = row[3].text
-        laps = row[4].text
-        duration = row[5].text
 
         races.append({
-            'race_id': race_id,
-            'race_name': race_name,
-            'date': race_date,
-            'race_winner': race_winner,
-            'race_winnter_tag': race_winner_tag,
-            'constructor': constructor,
-            'laps': laps,
-            'duration': duration
+            'race_id': race_link[6:race_link.find('/', 6, len(race_link)-1)],
+            'race_name': row[0].text,
+            'date': row[1].text,
+            'race_winner': row[2].text[0:len(row[2].text)-3].replace(u'\xa0', ' '),
+            'race_winnter_tag': row[2].text[len(row[2].text)-3::],
+            'constructor': row[3].text,
+            'laps': row[4].text,
+            'duration': row[5].text
         })
 
     return races
@@ -89,34 +69,28 @@ def fetch_session_results(year, race_name, session_type):
     
     return (race_results + filter_race_results(session_results, session_type))
 
+#change to inlcude practice/race/quali/etc
 def filter_race_results(session_results, session_type):
     results = []
     for result in session_results:
         #grab all <p> tags in the row
         row = result.find_all('p', ['f1-text font-titillium tracking-normal font-normal non-italic normal-case leading-none f1-text__micro text-fs-15px'])
-        pos = row[0].text
-        driver_number = row[1].text
-        driver = row[2].text[0:len(row[2].text)-3].replace(u'\xa0', ' ')
-        driver_tag = row[2].text[len(row[2].text)-3::]
-        constructor = row[3].text
-        laps_completed = row[4].text
-        time = row[5].text
-        points = row[6].text
+
         results.append({
-            'pos': pos,
-            'driver_number': driver_number,
-            'driver_name': driver,
-            'driver_tag': driver_tag,
-            'constructor': constructor,
-            'laps_completed': laps_completed,
-            'time': time,
-            'points': points
+            'pos': row[0].text,
+            'driver_number': row[1].text,
+            'driver_name': row[2].text[0:len(row[2].text)-3].replace(u'\xa0', ' '),
+            'driver_tag': row[2].text[len(row[2].text)-3::],
+            'constructor': row[3].text,
+            'laps_completed': row[4].text,
+            'time': row[5].text,
+            'points': row[6].text
         })
     return results  
 
 def main():
-    # a = fetch_session_results(2024, 'Italy', 'race')
-    a = fetch_races(2024)
+    # a = fetch_session_results(2024, 'Azerbaijan', 'race')
+    a = fetch_races(1950)
     for x in a:
         print(x)
 
